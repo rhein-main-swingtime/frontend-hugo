8a91098f7c21888e3f7ae9e8b7100dd5a15f4260
Felix Kakrow <felix.kakrow@check24.de>
2021-12-01 20:55:49 +0100

    Updates search

--- assets/scripts/Settings/RMSTApiUrls.ts
+++ assets/scripts/Settings/RMSTApiUrls.ts
@@ -1,6 +1,7 @@
 const basePath = 'https://api.rmswing.de'
 
 export default {
-    eventList: basePath + '/events/v1',
+    eventList: basePath + '/events/v1/list',
+    eventSearch: basePath + '/events/v1/search',
     filters: basePath + '/filters/v1/available'
 }
